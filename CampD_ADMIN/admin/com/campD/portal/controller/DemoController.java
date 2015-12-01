305c296113e258d4712fa9b1ed97378d27d6c664
Garrett <1015398431@qq.com>
2015-12-01 19:43:36 +0800

    导出excel案例。

--- CampD_ADMIN/admin/com/campD/portal/controller/DemoController.java
+++ CampD_ADMIN/admin/com/campD/portal/controller/DemoController.java
@@ -3,10 +3,14 @@
  */
 package com.campD.portal.controller;
 
+import java.io.OutputStream;
+import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,7 @@ import com.campD.portal.common.UserInfoHolder;
 import com.campD.portal.model.UserInfo;
 import com.campD.portal.service.DemoCacheService;
 import com.campD.portal.service.DemoService;
+import com.campD.portal.util.DateUtil;
 
 /**
  * @author Garrett Wang
@@ -59,4 +64,15 @@ public class DemoController extends BaseController{
 		
 		return demoService.register(reqMap);
     }
+	
+	@RequestMapping(value = "/excel/export.do")    
+    public void exportExcel(HttpServletRequest request, HttpServletResponse response) throws Exception {    
+        HSSFWorkbook wb = demoService.export();    
+        response.setContentType("application/vnd.ms-excel");    
+        response.setHeader("Content-disposition", "attachment;filename=student_"+DateUtil.fmtDate(new Date())+".xls");    
+        OutputStream ouputStream = response.getOutputStream();    
+        wb.write(ouputStream);    
+        ouputStream.flush();    
+        ouputStream.close();    
+   } 
 }
--- CampD_ADMIN/admin/com/campD/portal/service/DemoService.java
+++ CampD_ADMIN/admin/com/campD/portal/service/DemoService.java
@@ -1,7 +1,15 @@
 package com.campD.portal.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 import com.campD.portal.service.common.JsonClientService;
@@ -20,5 +28,47 @@ public class DemoService extends JsonClientService{
 	public Map register(Map reqMap) {
 		
 		return postForMap(SystemMessage.getString("userJsonServer") + "/add", reqMap);
+    }
+       
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	public HSSFWorkbook export() {    
+
+        String[] excelHeader = { "Sno", "Name", "Age"}; 
+        HSSFWorkbook wb = new HSSFWorkbook();    
+        HSSFSheet sheet = wb.createSheet("test");
+        sheet.setDefaultRowHeight((short)300); 
+        sheet.setDefaultColumnWidth(15);
+        HSSFRow row = sheet.createRow((int) 0);    
+        HSSFCellStyle style = wb.createCellStyle();    
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);    
+    
+        for (int i = 0; i < excelHeader.length; i++) {    
+            HSSFCell cell = row.createCell(i);    
+            cell.setCellValue(excelHeader[i]);    
+            cell.setCellStyle(style);    
+            sheet.autoSizeColumn(i);    
+        }    
+    
+        List<Map> list = new ArrayList();
+        list.add(getMapValue("1000","zhangsan","20"));    
+        list.add(getMapValue("1001","lisi","23"));    
+        list.add(getMapValue("1002","wangwu","25"));
+        for (int i = 0; i < list.size(); i++) {    
+            row = sheet.createRow(i + 1);    
+            Map student = list.get(i);    
+            row.createCell(0).setCellValue((String)student.get("Sno"));    
+            row.createCell(1).setCellValue((String)student.get("Name"));    
+            row.createCell(2).setCellValue((String)student.get("Age"));    
+        }    
+        return wb;    
+    } 
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	private Map getMapValue(String Sno, String Name, String Age){
+    	Map map = new HashMap();
+    	map.put("Sno", Sno);
+    	map.put("Name", Name);
+    	map.put("Age", Age);
+    	return map;
     }
 }
