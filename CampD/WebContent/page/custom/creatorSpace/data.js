fee8db57c8c88d37244099d346dad2c94f79c393
Garrett <1015398431@qq.com>
2016-01-08 17:30:10 +0800

    众创空间数据结构。

---
+++ CampD/WebContent/page/custom/creatorSpace/data.js
@@ -0,0 +1,34 @@
+/**
+ * 众创空间场地数据结构
+ */
+// 每个区域下面有哪些重创空间
+var areaLogoData={
+	"浦东新区":[
+        {"id":"0", "logo":"", "name":"", "mdn":""},
+        {"id":"1", "logo":"", "name":"", "mdn":""}
+	],
+	"黄浦区":[
+        {"id":"2", "logo":"", "name":"", "mdn":""},
+        {"id":"3", "logo":"", "name":"", "mdn":""}
+	]
+};
+
+// 每个重创空间下面有哪些场地
+var logoSpaceData={
+	"0":[
+        {"id":"0", "capacity":"", "name":"", "cost":"", "showImage":""},
+        {"id":"1", "capacity":"", "name":"", "cost":"", "showImage":""}
+	],
+	"1":[
+        {"id":"2", "capacity":"", "name":"", "cost":"", "showImage":""},
+        {"id":"3", "capacity":"", "name":"", "cost":"", "showImage":""}
+	],
+	"2":[
+        {"id":"4", "capacity":"", "name":"", "cost":"", "showImage":""},
+        {"id":"5", "capacity":"", "name":"", "cost":"", "showImage":""}
+	],
+	"3":[
+        {"id":"6", "capacity":"", "name":"", "cost":"", "showImage":""},
+        {"id":"7", "capacity":"", "name":"", "cost":"", "showImage":""}
+	]
+};
