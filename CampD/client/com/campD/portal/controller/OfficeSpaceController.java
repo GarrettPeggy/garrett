894456fa19251ebf34d52fd23077ccd8d61c18a3
Garrett <1015398431@qq.com>
2016-01-19 14:10:57 +0800

    给webapp添加了总空间模块。

---
+++ CampD/client/com/campD/portal/controller/OfficeSpaceController.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.campD.portal.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.campD.portal.common.JSONView;
+import com.campD.portal.common.PageInfo;
+import com.campD.portal.service.OfficeSpaceService;
+
+/**
+ * @author Garrett
+ *
+ */
+@Controller
+@RequestMapping("/officeSpace")
+public class OfficeSpaceController extends BaseController {
+
+	protected Logger logger = Logger.getLogger(getClass());
+	
+	@Autowired
+	private OfficeSpaceService officeSpaceService;
+
+	/**
+	 * 总空间列表页
+	 * @throws Exception
+	 */
+	@RequestMapping("/toList.do")
+	public String toList() throws Exception {
+		return "officeSpace/list";
+	}
+	
+	/**
+	 * 查询空间列表
+	 */
+	@RequestMapping("/list.do")
+	@ResponseBody
+	public JSONView getList(HttpServletResponse response, HttpServletRequest request) throws Exception {
+		
+		Map<String, Object> map = bindParamToMap(request);
+		PageInfo pageInfo=getPageInfo(request);
+		Map<?, ?> officeSpaceListMap = officeSpaceService.getList(map,pageInfo);
+		
+		return getSearchJSONView(officeSpaceListMap);
+	}
+	
+}
---
+++ CampD/client/com/campD/portal/service/OfficeSpaceService.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.campD.portal.service;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.campD.portal.common.PageInfo;
+import com.campD.portal.service.common.JsonClientService;
+import com.campD.portal.util.SystemMessage;
+
+/**
+ * @author Garrett
+ *
+ */
+@Service("officeSpaceService")
+public class OfficeSpaceService extends JsonClientService {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public Map getList(Map reqMap, PageInfo pageInfo) {
+		
+		return postForMap(SystemMessage.getString("officeSpaceJsonServer") + "/getList", reqMap, pageInfo, null, false);
+		
+    }
+}
